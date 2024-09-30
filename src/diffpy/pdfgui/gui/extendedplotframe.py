bcac8ba3e0c354b7fc8513ca3c5bc104b551da0b
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-09-30 17:49:21 -0400

    Remove two warnings due to plotting

--- src/diffpy/pdfgui/gui/extendedplotframe.py
+++ src/diffpy/pdfgui/gui/extendedplotframe.py
@@ -182,9 +182,10 @@ class ExtendedPlotFrame(wx.Frame):
         style -- the way curve should be plotted
         return:  internal reference to the newly added curve
         """
-        stylestr, properties = self.__translateStyles(style)
-        curveRef = self.subplot.plot(xData, yData, stylestr, **properties)[0]
-        self.subplot.legend(**legendBoxProperties())
+        properties = self.__translateStyles(style)
+        curveRef = self.subplot.plot(xData, yData, **properties)[0]
+        if "legend" in style:
+            self.subplot.legend(**legendBoxProperties())
         try:
             self.datalims[curveRef] = (min(xData), max(xData), min(yData), max(yData))
         except ValueError:
@@ -314,12 +315,11 @@ class ExtendedPlotFrame(wx.Frame):
             # not 'points', so line properties are required as well
             lineStyle = lineStyleDict.get(style["line"], "-")  # prefer solid
             lineWidth = style["width"]
-            stylestr += lineStyle
             properties.update({"color": color, "linestyle": lineStyle, "linewidth": lineWidth})
 
         if "legend" in style:
             properties["label"] = style["legend"]
-        return stylestr, properties
+        return properties
 
     def setTitle(self, wt, gt):
         """set graph labels
